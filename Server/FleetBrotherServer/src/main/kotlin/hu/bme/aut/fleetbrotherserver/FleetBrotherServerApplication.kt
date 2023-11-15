6d647896f6f84ca05c6620916ef256b85a4730df
Ádám Kalics <kalicsadam@gmail.com>
2023-11-15 22:09:26 +0100

    Initialize projects

---
+++ Server/FleetBrotherServer/src/main/kotlin/hu/bme/aut/fleetbrotherserver/FleetBrotherServerApplication.kt
@@ -0,0 +1,11 @@
+package hu.bme.aut.fleetbrotherserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FleetBrotherServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<FleetBrotherServerApplication>(*args)
+}
