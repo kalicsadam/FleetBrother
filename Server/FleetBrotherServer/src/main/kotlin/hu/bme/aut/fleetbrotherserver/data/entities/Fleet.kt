ae9ead78ba01ef844cdf60b2c6907b4c3784ecc5
Ádám Kalics <kalicsadam@gmail.com>
2023-11-23 00:48:12 +0100

    Refining entities and creating initial db script

--- Server/FleetBrotherServer/src/main/kotlin/hu/bme/aut/fleetbrotherserver/data/entities/Fleet.kt
+++ Server/FleetBrotherServer/src/main/kotlin/hu/bme/aut/fleetbrotherserver/data/entities/Fleet.kt
@@ -1,9 +1,19 @@
 package hu.bme.aut.fleetbrotherserver.data.entities
 
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
+
+@Entity
 data class Fleet(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Int,
     var name: String,
     var description: String,
 
-    var cars: MutableList<Car>,
+    @OneToMany(mappedBy = "fleet", orphanRemoval = true)
+    var cars: MutableList<Car> = mutableListOf(),
 )
