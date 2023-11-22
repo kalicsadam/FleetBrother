cb2471f46fc628c28b54a324cfea4abf6041edd5
od_ <asd@a.b>
2023-11-22 18:03:28 +0100

    Mqtt classes were renamed for clarity. Integration added for MQTT messaging.

--- Server/FleetBrotherServer/src/main/kotlin/hu/bme/aut/fleetbrotherserver/mqtt/MqttConfiguration.kt
+++ Server/FleetBrotherServer/src/main/kotlin/hu/bme/aut/fleetbrotherserver/mqtt/MqttConfiguration.kt
@@ -1,13 +1,59 @@
 package hu.bme.aut.fleetbrotherserver.mqtt
 
-import org.springframework.boot.context.properties.ConfigurationProperties
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions
+import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.integration.annotation.ServiceActivator
+import org.springframework.integration.channel.DirectChannel
+import org.springframework.integration.core.MessageProducer
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter
+import org.springframework.messaging.MessageChannel
+import org.springframework.messaging.MessageHandler
 
-@ConfigurationProperties(prefix="mqtt")
-data class MqttConfiguration (
-    val server: String,
-    val livez: String,
-    val measurementz: String,
-    val serverId: String,
-    val userName: String,
-    val password: String
-)
+
+@Configuration
+class MqttConfiguration(val conf: MqttParameters) {
+    val logger = LoggerFactory.getLogger(MqttConfiguration::class.java)!!
+
+    @Bean
+    fun mqttClientFactory(): MqttPahoClientFactory {
+        val factory = DefaultMqttPahoClientFactory()
+        val connOptions = MqttConnectOptions()
+        connOptions.serverURIs = arrayOf(conf.server)
+        connOptions.isCleanSession = true
+
+        factory.connectionOptions = connOptions
+        return factory
+    }
+
+    @Bean
+    fun mqttInputChannel(): MessageChannel {
+        return DirectChannel()
+    }
+
+    @Bean
+    fun inbound(): MessageProducer {
+        val adapter = MqttPahoMessageDrivenChannelAdapter(
+                conf.serverId,
+                mqttClientFactory(),
+                conf.livez,
+                conf.measurementz
+        )
+        adapter.setConverter(DefaultPahoMessageConverter())
+        adapter.setQos(1)
+        adapter.outputChannel = mqttInputChannel()
+        return adapter
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "mqttInputChannel")
+    fun handler(): MessageHandler {
+        return MessageHandler {
+            message -> logger.info(message.payload.toString())
+        }
+    }
+}
