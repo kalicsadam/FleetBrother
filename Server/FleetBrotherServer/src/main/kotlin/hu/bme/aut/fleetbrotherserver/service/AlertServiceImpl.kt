f62a24cf842e6d2eee5b9f21f2ebf7a22c381812
od_ <asd@a.b>
2023-11-26 21:45:33 +0100

    Double conversion check added

--- Server/FleetBrotherServer/src/main/kotlin/hu/bme/aut/fleetbrotherserver/service/AlertServiceImpl.kt
+++ Server/FleetBrotherServer/src/main/kotlin/hu/bme/aut/fleetbrotherserver/service/AlertServiceImpl.kt
@@ -87,8 +87,8 @@ class AlertServiceImpl(
             // Force cast was necessary because the compiler insisted that
             // the values could've been changed after the null check, because they're mutable.
             type == Type.NUMBER && (
-                    alert.minValue != null && field.asDouble() < alert.minValue!! ||
-                    alert.maxValue != null && field.asDouble() > alert.maxValue!!) -> true
+                    alert.minValue != null && field.isDouble && field.asDouble() < alert.minValue!! ||
+                    alert.maxValue != null && field.isDouble && field.asDouble() > alert.maxValue!!) -> true
             else -> false
         }
     }
